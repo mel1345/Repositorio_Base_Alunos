1d1401de0702fd486a30eb064b6421302322b73d
mel1345 <melissacezarmachado@gmail.com>
2025-09-11 13:45:40 -0300

    Add files via upload

---
+++ parte 2 .py
@@ -0,0 +1,16 @@
+numeros= [1, 2 ,3 ,4,]
+nomes= ["joaquim" , "maria"  "melissa"]
+
+nomes. append("joao")
+nomes. append ("joana")
+
+print (nomes[0]) 
+print (nomes [1])
+print (nomes [2])
+
+nomes.insert(2,"Teste")
+
+print(nomes)
+
+
+
